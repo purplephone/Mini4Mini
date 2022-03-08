0938011699d4aad1d0b6cc2e892d4556e5bf98b6
kr <hoylfree@gmail.com>
2022-03-08 13:54:27 +0900

    글 추가 수정

--- writing.py
+++ writing.py
@@ -11,20 +11,26 @@ ca = certifi.where()
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ldbjw.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
-@writing_api.route('/writing')
+@writing_api.route('/')
 def writing():
     writing_list = list(db.writing.find({}, {'_id': False}))
     return render_template('writing.html', writing_list=writing_list)
 
-@writing_api.route("/writing/post", methods=["POST"])
+def getGCount():
+    if not hasattr(g, 'count'):
+        g.count = 0
+        writing_list = list(db.writing.find({}, {'_id': False}))
+        for i in writing_list:
+            g.count = g.count if g.count > i['id'] else i['id']
+    return g.count
+
+@writing_api.route("/writing", methods=["POST"])
 def writing_post():
-    # db 저장
     title_receive = request.form['title_give']
     content_receive = request.form['content_give']
     category_receive = request.form['category_give']
-    writing_list = list(db.writing.find({}, {'_id': False}))
 
-    g.count = len(writing_list) + 1
+    g.count = getGCount()
     g.count += 1
     doc = {
         'id': getattr(g, "count", 0),
@@ -37,18 +43,16 @@ def writing_post():
     db.writing.insert_one(doc)
     return jsonify({'result': 'success', 'msg': '등록되었습니다.'})
 
-@writing_api.route("/writing/get", methods=["GET"])
+@writing_api.route("/writing", methods=["GET"])
 def writing_get():
-    # db 가져오기
     writing_list = list(db.writing.find({}, {'_id': False}))
     return jsonify({'writing':writing_list})
 
 @writing_api.route("/writing/delete", methods=["POST"])
 def writing_delete():
     id_receive = request.form['id_give']
-    print(id_receive)
     db.writing.delete_one({'id': int(id_receive)})
     return jsonify({'result': 'success', 'msg': '삭제되었습니다.'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=8080, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
