419b64157d5fb768a8ff2774a00f9e7464be62af
ggongdori <84629451+ggongdori@users.noreply.github.com>
2022-03-10 11:11:13 +0900

    주석
    주석

--- login.py
+++ login.py
@@ -16,26 +16,10 @@ login_api = Blueprint('login_api',__name__)
 client = MongoClient(DB_LINK, tlsCAFile=CA)
 db = client.dbsparta
 
-
 @login_api.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
-
-
-@login_api.route('/user/<username>')
-def user(username):
-    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-
-        user_info = db.users.find_one({"username": username}, {"_id": False})
-        return render_template('user.html', user_info=user_info, status=status)
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
-
 
 @login_api.route('/sign_in', methods=['POST'])
 def sign_in():
